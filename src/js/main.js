cbc5d240d7daf28523536a70f43a261abf2029c7
Bartosz Lewosz <bartosz.lewosz@gmail.com>
2019-11-03 17:48:06 +0000

    Add JS code for github repos

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,33 @@
-"use strict";
-
-// service worker registration - remove if you're not going to use it
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-
-// place your code below
-
-
 console.log(`Hello world!`);
 
+const url = "https://api.github.com/users/BartoszLewosz/repos?sort=updated&direction=desc";
 
+const repositories = document.querySelector('.projects__repositories--js');
+
+fetch(url)
+    .then(resp => resp.json())
+    .then(resp => {
+            const repos = resp;
+            for (const repo of repos) {
+                const { html_url, name, description, homepage } = repo;
+                repositories.innerHTML += `
+            <section class="projects__repo">
+                <div class="projects__container">
+                    <img class="projects__logo" src="assets/img/GitHub-Mark-64px.png" alt="github logo.">
+                    <h4 class="projects__title">${name}</h4>
+                    ${
+                        description ? `<p class="projects__content">${description}
+                        </p>` : ``
+                    } 
+                </div>
+                <div class="projects__footer">
+                ${
+                    homepage ? `<a class="projects__link projects__link--demo" href="${homepage}" target="_blank" rel="nofollow noreferrer" title="demo: ${name}.">Demo</a>`
+                    : ``
+                }
+                    <a class="projects__link projects__link--code" href="${html_url}" target="_blank" rel="nofollow noreferrer" title="source-code: ${name}.">Source
+                        Code</a>
+                </div>
+            </section>`
+        }
+    })
